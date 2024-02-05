1de35bfe7ae9e4039e5adfa2c8a510d46f74c9a1
domanjie <domanjie@gmail.com>
2024-02-05 18:57:14 +0100

    concluded  the looks for the EmailVerification page

--- src/App.jsx
+++ src/App.jsx
@@ -16,23 +16,23 @@ import { Routes, Route } from "react-router-dom"
 
 function App() {
   return (
-    <>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/sign-up" element={<SignUP />} />
-        <Route element={<AuthRequired />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/vendors" element={<VendorPage />} />
-          <Route path="/search" element={<SearchPage />} />
-          <Route path="/orders" element={<OrderPage />} />
-        </Route>
-        <Route path="/*" element={<NotFoundPage />}></Route>
-      </Routes>
-    </>
     // <>
-    //   <EmailVerification></EmailVerification>
+    //   <Routes>
+    //     <Route path="/login" element={<Login />} />
+    //     <Route path="/sign-up" element={<SignUP />} />
+    //     <Route element={<AuthRequired />}>
+    //       <Route path="/" element={<Home />} />
+    //       <Route path="/cart" element={<Cart />} />
+    //       <Route path="/vendors" element={<VendorPage />} />
+    //       <Route path="/search" element={<SearchPage />} />
+    //       <Route path="/orders" element={<OrderPage />} />
+    //     </Route>
+    //     <Route path="/*" element={<NotFoundPage />}></Route>
+    //   </Routes>
     // </>
+    <>
+      <EmailVerification></EmailVerification>
+    </>
   )
 }
 
--- src/singUp/EmailVerificationPage.jsx
+++ src/singUp/EmailVerificationPage.jsx
@@ -2,40 +2,40 @@ import { useEffect, useRef } from "react"
 import "./styles.css"
 
 const EmailVerification = () => {
-  const inputRefArr = Array(6).fill(useRef())
+  const inputRefArr = Array(6)
+    .fill()
+    .map(() => useRef())
   useEffect(() => {
-    const inputs = inputRefArr.map((inputRefArr) => inputRefArr.current)
-    console.log(inputs)
+    const inputs = inputRefArr.map((inputRef) => inputRef.current)
     for (let i = 0; i < inputs.length; i++) {
-      inputs[i].addEventListener("input", function (event) {
-        console.log("called added oo")
-        // if (event.key === "Backspace") {
-        //   inputs[i].value = ""
-        //   if (i !== 0) inputs[i - 1].focus()
-        // } else {
-        //   if (i === inputs.length - 1 && inputs[i].value !== "") {
-        //     return true
-        //   } else if (event.keyCode > 47 && event.keyCode < 58) {
-        //     inputs[i].value = event.key
-        //     if (i !== inputs.length - 1) inputs[i + 1].focus()
-        //     event.preventDefault()
-        //   } else if (event.keyCode > 64 && event.keyCode < 91) {
-        //     inputs[i].value = String.fromCharCode(event.keyCode)
-        //     if (i !== inputs.length - 1) inputs[i + 1].focus()
-        //     event.preventDefault()
-        //   }
-        // }
+      inputs[i].addEventListener("keydown", function (event) {
+        if (event.key === "Backspace") {
+          inputs[i].value = ""
+          if (i !== 0) inputs[i - 1].focus()
+        } else {
+          if (i === inputs.length - 1 && inputs[i].value !== "") {
+            return true
+          } else if (event.keyCode > 47 && event.keyCode < 58) {
+            inputs[i].value = event.key
+            if (i !== inputs.length - 1) inputs[i + 1].focus()
+            event.preventDefault()
+          } else if (event.keyCode > 64 && event.keyCode < 91) {
+            inputs[i].value = String.fromCharCode(event.keyCode)
+            if (i !== inputs.length - 1) inputs[i + 1].focus()
+            event.preventDefault()
+          }
+        }
       })
     }
-    console.log("done")
   }, [])
 
   return (
     <section className="email-verification">
-      <p>
+      <h2>Almost Done...</h2>
+      <p className="email-verification-p1">
         We Sent Verification code to
-        <span className="s-l-link">zendaya@gmail.com</span>, please check inbox
-        and enter code below.
+        <strong> zendaya@gmail.com</strong>, please check inbox and enter code
+        below.
       </p>
       <div>
         <input maxLength={1} type="text" ref={inputRefArr[0]} />
@@ -45,6 +45,7 @@ const EmailVerification = () => {
         <input maxLength={1} type="text" ref={inputRefArr[4]} />
         <input maxLength={1} type="text" ref={inputRefArr[5]} />
       </div>
+      <button className="submit-btn email-v-btn">verify</button>
       <p>
         didn’t recieve an email? <span className="s-l-link">resend</span>{" "}
       </p>
