3d715c05f1aff3bcd2abe5e9ff1ed998a53c8b20
domanjie <domanjie@gmail.com>
2024-09-05 19:55:14 +0100

    added luhn check algo for credit card validation

---
+++ src/infra/LuhnCheck.js
@@ -0,0 +1,14 @@
+export const luhnCheck = (val) => {
+  let sum = 0
+  for (let i = 0; i < val.length; i++) {
+    let intVal = parseInt(val.substr(i, 1))
+    if (i % 2 == 0) {
+      intVal *= 2
+      if (intVal > 9) {
+        intVal = 1 + (intVal % 10)
+      }
+    }
+    sum += intVal
+  }
+  return sum % 10 == 0
+}
